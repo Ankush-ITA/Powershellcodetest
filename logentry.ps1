72cb9c393c58dfbe10acb0c4eaef92c1c63c51fb
Ankush-ITA <127494609+Ankush-ITA@users.noreply.github.com>
2024-03-12 00:32:55 +0530

    Delete logentry.ps1

--- logentry.ps1
+++
@@ -1,9 +0,0 @@
-param ([string]$LogMessage, [string]$LogFile)
-    try {
-        $TimeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-        $LogEntry = "$TimeStamp - $LogMessage"  
-        Add-Content -Path $LogFile -Value $LogEntry
-    }
-    catch {
-        Write-Error "Error occurred while writing to the log file: $_"
-    }
